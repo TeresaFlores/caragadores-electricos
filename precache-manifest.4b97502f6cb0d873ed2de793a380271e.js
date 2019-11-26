fa2c8c5a5bb7218fa8bd46a4daf289aa74376651
TeresaFlores <teresaflores1730@gmail.com>
2019-11-25 18:13:44 -0600

    Updates

---
+++ precache-manifest.4b97502f6cb0d873ed2de793a380271e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f40e4feabc3f9577a11b90dc4897dc49",
+    "url": "./index.html"
+  },
+  {
+    "revision": "0c10eda796ec12692449",
+    "url": "./static/css/2.8b70035b.chunk.css"
+  },
+  {
+    "revision": "79634b06866e77b1a645",
+    "url": "./static/css/main.45230856.chunk.css"
+  },
+  {
+    "revision": "0c10eda796ec12692449",
+    "url": "./static/js/2.2ef082e3.chunk.js"
+  },
+  {
+    "revision": "79634b06866e77b1a645",
+    "url": "./static/js/main.fef6e1bb.chunk.js"
+  },
+  {
+    "revision": "33f58d7dbeba69ff4d31",
+    "url": "./static/js/runtime-main.a4a91e9b.js"
+  }
+]);
